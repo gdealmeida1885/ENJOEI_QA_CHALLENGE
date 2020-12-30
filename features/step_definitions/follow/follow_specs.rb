fdce60191f9e2ca0edf872b33540360e014071e4
Gabriel Almeida <gdealmeida@MacBook-Air-de-Gabriel.local>
2020-12-29 23:10:27 -0300

    commented on follow steps since its not being executed

--- features/step_definitions/follow/follow_specs.rb
+++ features/step_definitions/follow/follow_specs.rb
@@ -1,15 +1,15 @@
-Given("I have {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# Given("I have {string}") do |string|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
 
-Given("I do login") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# Given("I do login") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
 
-When("I find a store that I like and follow it") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# When("I find a store that I like and follow it") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
 
-Then("I must see my profile on the followers list") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# Then("I must see my profile on the followers list") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
